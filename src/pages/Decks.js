983bfca3b5564fbf01b3fc401a0c6e672152b0ce
Oleh Lutsenko <oleh.lutsenko@tobii.com>
2018-10-31 14:54:13 +0100

    add pagination to decks page

--- src/pages/Decks.js
+++ src/pages/Decks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import firebase, { db } from '../firebase';
 import { List } from 'immutable';
 import DeckOverview from '../components/DeckOverview';
@@ -7,12 +7,14 @@ import FloatingActionButton from '../components/FloatingActionButton';
 import AddIcon from '@material-ui/icons/Add';
 import { withRouter } from 'react-router-dom';
 import FactionsFilterToggle from '../components/FactionsFilterToggle';
-import { Typography, Button } from '@material-ui/core';
+import { Typography, Button, ButtonBase } from '@material-ui/core';
 import { filterFactionByIdRange } from '../data/index';
 import { connect } from 'react-redux';
 import { SET_FACTIONS_FILTER } from '../reducers/decksFilters';
 import { withStyles } from '@material-ui/core/styles';
 import classnames from 'classnames';
+import { Subject, Observable, combineLatest, merge, of } from 'rxjs';
+import { switchMap, mergeAll, take } from 'rxjs/operators';
 
 const uuid4 = require('uuid/v4');
 
@@ -40,33 +42,77 @@ const styles = theme => ({
     },
 });
 
+const pagesFromTotalPages = totalPages => {
+    const pages = [];
+    for(let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+    }
+
+    return pages;
+}
+
+class PaginationButton extends PureComponent {
+    render() {
+        return (
+            <ButtonBase 
+                style={{ 
+                    width: '3rem', 
+                    backgroundColor: this.props.selected ? '#3B9979' : '#8FC6B3',
+                    padding: '.5rem',
+                    borderRadius: '.2rem',
+                    margin: '0 0 0 .3rem'}}
+                onClick={this.handleClick}>
+                <Typography variant="button" style={{ color: 'white' }}>{this.props.pageNumber}</Typography>        
+            </ButtonBase>            
+        );
+    }
+
+    handleClick = () => {
+        this.props.onPageNumberClick(this.props.pageNumber);
+    }
+}
+
+class Pagination extends PureComponent {
+    state = {
+        currentPage: this.props.currentPage,
+        totalPages: pagesFromTotalPages(this.props.totalPages)
+    }
+
+    componentDidUpdate = prevProps => {
+        if(this.props.totalPages !== prevProps.totalPages) {
+            this.setState({ totalPages: pagesFromTotalPages(this.props.totalPages) });
+        }
+    }
+
+    render() {
+        return (
+            <div style={{ display: 'flex', flexFlow: 'row wrap', margin: '1rem' }}>
+            {
+                this.state.totalPages.map(p => 
+                    <PaginationButton key={p} pageNumber={p} selected={p === this.state.currentPage} onPageNumberClick={this.selectPage} />)
+            }
+            </div>
+        );
+    }
+
+    selectPage = page => {
+        this.setState({ currentPage: page });
+        this.props.onCurrentPageChange(page);
+    }
+}
+
 class Decks extends Component {
     state = {
-        decks: new List(),
-        loading: false
+        decks: [],
+        loading: false,
+        currentPage: 0,
+        pageStart: 0,
+        pageLength: 10,
+        totalPages: 0
     }
 
     componentDidMount = async () => {
         await this._reloadWithFilters();
-    }
-
-    _reloadDecks = async query => {
-        try {
-            this.setState({ loading: true });
-            query.forEach(async doc => {
-                const deck = doc.data();
-                if(deck.author !== 'Anonymous') {
-                    const userProfileRef = await db.collection('users').doc(deck.author).get();
-                    this.setState(state => ({ decks: state.decks.push({...deck, id: doc.id, author: userProfileRef.data().displayName})} ));        
-                } else {
-                    this.setState(state => ({ decks: state.decks.push({...deck, id: doc.id })}));
-                }
-            });
-
-            this.setState({loading: false});
-        } catch(error) {
-            console.log(error);
-        }
     }
 
     _reloadWithFilters = async () => {
@@ -82,23 +128,40 @@ class Decks extends Component {
             }
         });
 
-        for(let query of queries) {
-            const querySnapshot = await query.get();
-            querySnapshot.forEach(async doc => {
-                const deck = doc.data();
-                const created = deck.created.toDate();
-                if(deck.author !== 'Anonymous') {
-                    const userProfileRef = await db.collection('users').doc(deck.author).get();
-                    this.setState(state => ({ 
-                        loading: false,
-                        decks: state.decks.push({ ...deck, id: doc.id, created: created, author: userProfileRef.data().displayName })}));
-                } else {
-                    this.setState(state => ({ 
-                        loading: false,
-                        decks: state.decks.push({ ...deck, id: doc.id, created: created})}));
-                }
+        const observables = [];
+        for(let q of queries) {
+            const observable = new Subject();
+            q.onSnapshot(qs => {
+                const data = qs.docs.map(d => ({id: d.id, ...d.data()}));
+                observable.next(data);
             });
+            observables.push(observable);
         }
+
+        const combined = combineLatest(...observables);
+        const mapped = combined.pipe(switchMap(values => {
+            const reduced = values.reduce((acc, v) => [...acc, ...v], []);
+            return of(reduced);
+        }));
+        
+        this.subscription = mapped.subscribe(async result => {
+            const decks = [];
+            for(let r of result) {
+                const created = r.created.toDate();
+                if(r.author !== 'Anonymous') {
+                    const userProfileRef = await db.collection('users').doc(r.author).get();
+                    decks.push({ ...r, created: created, author: userProfileRef.data().displayName });
+                } else {
+                    decks.push({ ...r, created: created});
+                }
+            }
+
+            this.setState({ loading: false, currentPage: 1, totalPages: Math.ceil(result.length / 10), decks: decks, pageStart: 0 });
+        });
+    }
+
+    componentWillUnmount = () => {
+        this.subscription.unsubscribe();
     }
 
     render() {
@@ -125,9 +188,16 @@ class Decks extends Component {
                         ) 
                     }
                     {
-                        this.state.decks
-                            .sortBy(d => d.created, (date1, date2) => date2 - date1)
-                            .map(d => <DeckOverview key={uuid4()} isEditable={false} {...d} />)
+                        this.state.totalPages > 1 && (
+                            <Pagination currentPage={this.state.currentPage} 
+                                totalPages={this.state.totalPages} 
+                                onCurrentPageChange={this.updateCurrentPage} />
+                        )
+                    }
+                    {
+                        this.state.decks.slice(this.state.pageStart, this.state.pageStart + this.state.pageLength).map(d => (
+                            <DeckOverview key={uuid4()} isEditable={false} {...d} />
+                        ))
                     }
                 </div>
                 <FloatingActionButton isEnabled onClick={() => history.push('/newdeck')}>
@@ -135,6 +205,10 @@ class Decks extends Component {
                 </FloatingActionButton>
             </div>
         );
+    }
+
+    updateCurrentPage = page => {
+        this.setState(state => ({ currentPage: page, pageStart: (page - 1) * state.pageLength }));
     }
 }
 
