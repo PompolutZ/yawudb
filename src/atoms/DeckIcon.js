dd132a4d8ae6a46bd8acc9637939e17a6eff0c9a
Oleh Lutsenko <oleh.lutsenko@tobii.com>
2018-11-26 07:58:51 +0100

    fix deckicon for N_ factions

--- src/atoms/DeckIcon.js
+++ src/atoms/DeckIcon.js
@@ -3,11 +3,12 @@ import React, { PureComponent } from 'react';
 class DeckIcon extends PureComponent {
     render() {
         const { width, height, faction} = this.props;
+        const strippedFaction = faction.startsWith('n_') ? faction.substr(2) : faction;
         return (
             <img id="factionDeckIcon" 
                 style={{width: width, height: height, margin: '0 .3rem 0 0', flex: '0 0 auto'}} 
                 alt={`${faction}`} 
-                src={`/assets/icons/${faction}-deck.png`} />
+                src={`/assets/icons/${strippedFaction}-deck.png`} />
         );
     }
 }
