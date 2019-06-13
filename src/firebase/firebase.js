d4e63bb13213480db6acd4bd4634e6d0a37d2e3e
Oleh Lutsenko <oleh.lutsenko@ad.exopen.se>
2019-06-13 12:53:38 +0200

    add react-app-env, reconfigure to use environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,30 @@ import app from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/database'
 import 'firebase/auth'
-import { prodConfig, devConfig } from './config';
+// import { prodConfig, devConfig } from './config';
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+}
 
-const config = process.env.REACT_APP_STAGE === 'prod' ? prodConfig : devConfig
+// const prodConfig = {
+//     apiKey: process.env.REACT_APP_PROD_API_KEY,
+//     authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+// }
+
+// const config = process.env.REACT_APP_ENVIRONMENT === 'prod' ? prodConfig : devConfig
 
 class Firebase {
     constructor() {
+        console.log('HERE!', process.env);
         app.initializeApp(config)
 
         this.auth = app.auth()
