070df581f3d018b9057be8000b450f9026231b3f
PompolutZ <pompolutz@gmail.com>
2020-11-21 18:00:53 +0100

    change ExpansionIcon component

--- src/atoms/ExpansionIcon.js
+++ src/atoms/ExpansionIcon.js
@@ -1,56 +1,24 @@
-import React, { PureComponent } from "react";
-// import { setsIndex, setsNames } from "../data/index";
-import { withStyles } from "@material-ui/core/styles";
-import { getSetNameById } from "../data/wudb";
+import React from "react";
 
-const styles = (theme) => ({
-    root: {
-        display: "flex",
-        alignItems: "center",
-    },
-
-    small: {
-        width: "1rem",
-        height: "1rem",
-    },
-
-    medium: {
-        width: "2rem",
-        height: "2rem",
-    },
-
-    large: {
-        width: "3rem",
-        height: "3rem",
-    },
-});
-
-class ExpansionIcon extends PureComponent {
-    render() {
-        const { classes, set, variant, setName } = this.props;
-        // const setName = getSetNameById(+set);
-        return (
-            <div className={classes.root}>
-                <picture>
-                    <source
-                        type="image/webp"
-                        srcSet={`/assets/icons/${setName}-icon.webp`}
-                    />
-                    <img
-                        src={`/assets/icons/${setName}-icon-24.png`}
-                        alt={`${setName}`}
-                        className={
-                            variant === "large"
-                                ? classes.large
-                                : variant === "medium"
-                                ? classes.medium
-                                : classes.small
-                        }
-                    />
-                </picture>
-            </div>
-        );
-    }
+function ExpansionIcon({ setName, variant, ...rest }) {
+    const size = 
+        variant == 'large' ? "w-12 h-12" : 
+        variant == 'medium' ? "w-8 h-8" :
+        "w-4 h-4";
+        
+    return (
+        <picture>
+            <source
+                type="image/webp"
+                srcSet={`/assets/icons/${setName}-icon.webp`}
+            />
+            <img
+                src={`/assets/icons/${setName}-icon-24.png`}
+                alt={`${setName}`}
+                className={`${size} ${rest.className}`}
+            />
+        </picture>
+    );
 }
 
-export default withStyles(styles)(ExpansionIcon);
+export default ExpansionIcon;
