f85d27e867ebfd0fbaecc074a1358fc86a2eba83
Oleh Lutsenko <pompolutz@gmail.com>
2021-08-10 23:39:19 +0200

    remove more old components

--- src/atoms/ToggableExpansionIcon.jsx
+++ src/atoms/ToggableExpansionIcon.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import ExpansionIcon from "./ExpansionIcon";
 import classnames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
