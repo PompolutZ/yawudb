cb18e4f481b1f8595db000322a354a48e251616c
Oleh Lutsenko <pompolutz@gmail.com>
2023-05-10 23:15:35 +0200

    replace hero image

--- src/v2/components/HeroImage.jsx
+++ src/v2/components/HeroImage.jsx
@@ -5,14 +5,14 @@ function HeroImage() {
         <picture>
             <source
                 type="image/avif"
-                srcSet="/assets/gnarlwood_bg_xs.webp 375w, /assets/gnarlwood_bg_lg.webp 1000w"
+                srcSet="/assets/wyrdhollow_bg_xs.webp 375w, /assets/wyrdhollow_bg_lg.webp 1000w"
             />
             <source
                 type="image/webp"
-                srcSet="/assets/gnarlwood_bg_xs.webp 375w, /assets/gnarlwood_bg_lg.webp 1000w"
+                srcSet="/assets/wyrdhollow_bg_xs.webp 375w, /assets/wyrdhollow_bg_lg.webp 1000w"
             />
             <img
-                srcSet="/assets/gnarlwood_bg_xs.jpg 375w, /assets/gnarlwood_bg_lg.jpg 1000w"
+                srcSet="/assets/wyrdhollow_bg_xs.jpg 375w, /assets/wyrdhollow_bg_lg.jpg 1000w"
                 style={{
                     width: "100%",
                     height: "70%",
